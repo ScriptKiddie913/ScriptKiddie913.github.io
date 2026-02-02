c243027fc2c895be17dba1f986de9567fe24bcc9
ScriptKiddie913 <sagnik.saha.raptor@gmail.com>
2026-02-03 00:51:47 +0530

    test

--- next.config.js
+++ next.config.js
@@ -1,16 +1,40 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  reactStrictMode: true,
+  swcMinify: true,
   experimental: {
     appDir: true,
   },
   images: {
-    domains: ['images.credly.com', 'www.opswat.com', 'upload.wikimedia.org', 'brm-workforce.oracle.com'],
+    domains: [
+      'images.credly.com', 
+      'www.opswat.com', 
+      'upload.wikimedia.org', 
+      'brm-workforce.oracle.com'
+    ],
     unoptimized: true,
   },
-  env: {
-    RESEND_API_KEY: process.env.RESEND_API_KEY,
-  }
+  async headers() {
+    return [
+      {
+        source: '/api/(.*)',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
