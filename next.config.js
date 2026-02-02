27506671212aec66ba11f1bef640ca0ba6fed420
ScriptKiddie913 <sagnik.saha.raptor@gmail.com>
2026-02-03 00:43:06 +0530

    test

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   experimental: {
     appDir: true,
   },
   images: {
     domains: ['images.credly.com', 'www.opswat.com', 'upload.wikimedia.org', 'brm-workforce.oracle.com'],
+    unoptimized: true,
   },
+  env: {
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
+  }
 }
 
 module.exports = nextConfig
